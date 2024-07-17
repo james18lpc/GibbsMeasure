b38cb7f32d792813cd470d3e5deed4b2808778ab
Yaël Dillies <yael.dillies@gmail.com>
2024-07-17 08:39:33 +0000

    Update lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,18 +1,18 @@
 import Lake
 open Lake DSL
 
-package «summer_2024» where
-  -- Settings applied to both builds and interactive editing
+package GibbsMeasure where
   leanOptions := #[
-    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
-  ]
-  -- add any additional package configuration options here
+    ⟨`autoImplicit, false⟩, -- prevents typos to be interpreted as new free variables
+    ⟨`relaxedAutoImplicit, false⟩, -- prevents typos to be interpreted as new free variables
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`pp.proofs.withType, false⟩]
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «GibbsMeasure» where
+lean_lib GibbsMeasure where
   -- add any library configuration options here
 
 require checkdecls from git "https://github.com/PatrickMassot/checkdecls.git"
