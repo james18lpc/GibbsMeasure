2043815a65081ca0cddd85ecd97da8faeb9a8557
Yaël Dillies <yael.dillies@gmail.com>
2024-07-18 11:57:30 +0000

    Update GibbsMeasure

--- GibbsMeasure.lean
+++ GibbsMeasure.lean
@@ -1,7 +1,7 @@
 import GibbsMeasure.Basic
-import GibbsMeasure.IsCondKernel
 import GibbsMeasure.Mathlib.MeasureTheory.Constructions.Prod.Basic
 import GibbsMeasure.Mathlib.MeasureTheory.MeasurableSpace.Basic
+import GibbsMeasure.Mathlib.Probability.Kernel.Disintegration.Basic
 import GibbsMeasure.Prereqs.CylinderEvent
 import GibbsMeasure.Prereqs.Extend
 import GibbsMeasure.Prereqs.Kernel.Proper
