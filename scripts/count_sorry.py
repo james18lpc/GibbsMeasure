0a473f14593e71d67356d7bb05dc90d219e5775a
Yaël Dillies <yael.dillies@gmail.com>
2024-10-17 11:05:02 +0000

    Fix sorry-count script

--- scripts/count_sorry.py
+++ scripts/count_sorry.py
@@ -2,15 +2,10 @@ from glob import glob
 import os
 import re
 
-def ident_to_word(ident: str):
-    ident = ident.replace("_", " ")
-    return ident[0].upper() + ident[1:]
-
-def pretty_file(file: str):
-    segments = file[4:].replace(".lean", "").split("/")
-    name = ": ".join(ident_to_word(s) for s in segments)
-    url = f"https://github.com/james18lpc/GibbsMeasure/blob/master/{file}"
-    return f"[{name}]({url})"
+def pretty_file(filename: str):
+    prettyname = filename.replace(".lean", "").replace("/", ".")
+    url = f"https://github.com/james18lpc/GibbsMeasure/blob/master/{filename}"
+    return f"[{filename}]({url})"
 
 files = [file for tree in os.walk("GibbsMeasure") for file in glob(os.path.join(tree[0], '*.lean'))]
 sorries = {}
