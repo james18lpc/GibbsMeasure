2183b8a324409b80d3ab30672b76244d0c0bbff3
Yaël Dillies <yael.dillies@gmail.com>
2025-05-07 10:08:25 +0000

    more cleanup

--- GibbsMeasure/Prereqs/Kernel/Proper.lean
+++ GibbsMeasure/Prereqs/Kernel/Proper.lean
@@ -17,45 +17,41 @@ variable {X : Type*} {𝓑 𝓧 : MeasurableSpace X} {π : Kernel[𝓑, 𝓧] X 
   {f g : X → ℝ≥0∞} {x₀ : X}
 
 private lemma IsProper.integral_indicator_mul_indicator (hπ : IsProper π) (h𝓑𝓧 : 𝓑 ≤ 𝓧)
-    (hA : MeasurableSet[𝓧] A) (hB : MeasurableSet[𝓑] B) :
+  (hA : MeasurableSet[𝓧] A) (hB : MeasurableSet[𝓑] B) :
     ∫ x, (B.indicator 1 x * A.indicator 1 x : ℝ) ∂(π x₀) =
       B.indicator 1 x₀ * ∫ x, A.indicator 1 x ∂(π x₀) := by
   calc
         ∫ x, (B.indicator 1 x * A.indicator 1 x : ℝ) ∂(π x₀)
     _ = (∫⁻ x, .ofReal (B.indicator 1 x * A.indicator 1 x) ∂π x₀).toReal :=
       integral_eq_lintegral_of_nonneg_ae (.of_forall <| by simp [indicator_nonneg, mul_nonneg])
-        (by measurability)
+      (by measurability)
     _ = (∫⁻ x, B.indicator 1 x * A.indicator 1 x ∂π x₀).toReal := by
       simp [ofReal_mul, indicator_nonneg]
     _ = (B.indicator 1 x₀ * ∫⁻ x, A.indicator 1 x ∂π x₀).toReal := by
       rw [hπ.lintegral_mul h𝓑𝓧 (by measurability) (by measurability)]
     _ = B.indicator 1 x₀ * ∫ x, A.indicator 1 x ∂π x₀ := by
       rw [integral_eq_lintegral_of_nonneg_ae (.of_forall <| by simp [indicator_nonneg, mul_nonneg])
-        (by measurability)]
+      (by measurability)]
       simp [ofReal_mul]
 
-lemma indicator_eq_mul_one (f: X → ℝ) (B: Set X): B.indicator f = (B.indicator 1)*f := by
+lemma indicator_eq_mul_one (f: X → ℝ) (B: Set X) : B.indicator f =B.indicator 1 * f:= by
   ext x
-  by_cases hxiB:x ∈ B <;> simp [hxiB]
+  by_cases hxiB: x ∈ B <;> simp [hxiB]
 
 lemma integral_indicator_of_mul_indicator (f: X → ℝ) (B: Set X) {μ : Measure X} :
   ∫ (x : X), (B.indicator 1 x) * f x ∂μ = ∫ (x : X), (B.indicator (1 * f)) x ∂μ := by
-   simp
-   rw [indicator_eq_mul_one f B]
-   rfl
+   simp [← indicator_mul_const]; rfl
 
 private lemma IsProper.integral_indicator_mul {f : X → ℝ} (hπ : IsProper π) (h𝓑𝓧 : 𝓑 ≤ 𝓧)
-    (hf : Integrable[𝓧] f (π x₀)) (hB : MeasurableSet[𝓑] B):
-      ∫ x, (B.indicator 1 x) *  (f x)  ∂(π x₀) =
-        (B.indicator 1 x₀) * (∫ x, (f x) ∂(π x₀)) := by
-  apply Integrable.induction (μ:=(π x₀)) (fun (h:X→ℝ) ↦ Integrable[𝓧] h (π x₀) →
-    ∫ x, (B.indicator 1 x) * (h x) ∂(π x₀) = B.indicator 1 x₀ * ∫ x, h x ∂(π x₀))
-  · intro c S hmS bpS _
-    rw [integral_indicator_const (μ:= (π x₀)) c (hmS)]
+  (hf : Integrable[𝓧] f (π x₀)) (hB : MeasurableSet[𝓑] B) :
+      ∫ x, B.indicator 1 x * f x ∂(π x₀) = B.indicator 1 x₀ * ∫ x, f x ∂(π x₀) := by
+  refine Integrable.induction _ ?_ ?_ ?_ ?_ hf
+  · intro c S hmS bpS
+    rw [integral_indicator_const (μ := π x₀) c hmS]
     simp_rw [← smul_indicator_one_apply, smul_eq_mul, ← mul_assoc]
     simp only [mul_comm, mul_assoc]
     rw [integral_const_mul, integral_indicator_mul_indicator hπ h𝓑𝓧 hmS hB, ← mul_assoc]
-    rw [integral_indicator_one (μ:= (π x₀)) (hmS)]
+    rw [integral_indicator_one (μ := π x₀) hmS]
     ring
   · intro f g disj intf intg
     simp_all
@@ -64,30 +60,22 @@ private lemma IsProper.integral_indicator_mul {f : X → ℝ} (hπ : IsProper π
       rhs
       intro x
       rw [mul_add]
-    have disj':  Disjoint (Function.support ((B.indicator 1)*f))
-      (Function.support ((B.indicator 1)*g)) := by
-      simp
-      refine (Disjoint.inter_left' B ?_)
-      refine (Disjoint.inter_right' B ?_)
-      exact disj
-    have intBf: Integrable (μ := (π x₀))  (((B.indicator 1)*f) : X → ℝ ) := by
+    have intBf: Integrable (μ := (π x₀)) (B.indicator 1 * f : X → ℝ ) := by
       have t1 := Integrable.indicator intf (h𝓑𝓧 B hB)
-      have t2:B.indicator f = (B.indicator 1)*f := by
+      have t2:B.indicator f =B.indicator 1 * f:= by
         ext x
-        by_cases hxiB:x ∈ B
+        by_cases hxiB: x ∈ B
         · simp [hxiB]
         · simp [hxiB]
       rw [t2] at t1
       exact t1
-    have intBg: Integrable (μ := (π x₀))  (((B.indicator 1)*g) : X → ℝ ) := by
+    have intBg: Integrable (μ := (π x₀)) (B.indicator 1 * g : X → ℝ ) := by
       have t1 := Integrable.indicator intg (h𝓑𝓧 B hB)
       rw [indicator_eq_mul_one g B] at t1
       exact t1
     rw [integral_add intf intg]
-    have fun_add: ∀x, (B.indicator 1 x * f x + B.indicator 1 x * g x
-      = (((B.indicator 1)*f):X → ℝ) x + (((B.indicator 1)*g):X → ℝ) x ) := by
-      intro x
-      simp
+    have fun_add x : B.indicator 1 x * f x + B.indicator 1 x * g x
+      = (B.indicator 1 * f : X → ℝ) x + (B.indicator 1 * g : X → ℝ) x := by simp
     simp [fun_add]
     simp_rw [integral_add intBf intBg]
     simp
@@ -96,7 +84,7 @@ private lemma IsProper.integral_indicator_mul {f : X → ℝ} (hπ : IsProper π
   · conv =>
       rhs
       rhs
-      intro f intf
+      intro f
       rw [← norm_sub_eq_zero_iff]
     simp [L1.integrable_coeFn]
     refine IsClosed.preimage (f := fun g:↥(Lp ℝ 1 (π x₀)) ↦ ∫ (x : X), B.indicator 1 x * g x
@@ -104,15 +92,14 @@ private lemma IsProper.integral_indicator_mul {f : X → ℝ} (hπ : IsProper π
     · refine Continuous.sub ?_ ?_
       · simp only [integral_indicator_of_mul_indicator]
         simp [integral_indicator (h𝓑𝓧 B hB)]
-        refine MeasureTheory.continuous_setIntegral (E:=ℝ) B
+        refine MeasureTheory.continuous_setIntegral (E := ℝ) B
       · apply Continuous.comp'
         · apply continuous_mul_left
         · simp only [← L1.integral_eq_integral]
           refine L1.continuous_integral
     · exact isClosed_singleton
   · intro k g hfAEg Intf hfIND
-    have t1 := hfIND Intf
-    intro intG
+    have t1 := hfIND
     rw [← integral_congr_ae hfAEg]
     have :  B.indicator (1 * g) =ᵐ[π x₀] (B.indicator 1) *k := by
       simp
@@ -120,11 +107,9 @@ private lemma IsProper.integral_indicator_mul {f : X → ℝ} (hπ : IsProper π
       exact Filter.EventuallyEq.indicator (id (Filter.EventuallyEq.symm hfAEg))
     rw [integral_indicator_of_mul_indicator g B, ← integral_congr_ae this.symm]
     exact t1
-  · exact hf
-  · exact hf
 
 lemma IsProper.integral_mul (hπ : IsProper π) (h𝓑𝓧 : 𝓑 ≤ 𝓧) (f g : X → ℝ) (x₀ : X)
-    (hf : Integrable[𝓧] f (π x₀)) (hg : Integrable[𝓑] (f * g) (@Measure.map _ _ 𝓧 𝓑 id (π x₀))) :
+  (hf : Integrable[𝓧] f (π x₀)) (hg : Integrable[𝓑] (f * g) (@Measure.map _ _ 𝓧 𝓑 id (π x₀))) :
     ∫ x, f x * g x ∂(π x₀) = g x₀ * ∫ x, f x ∂(π x₀) := by
   --Integrable.induction
   sorry
